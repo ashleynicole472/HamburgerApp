42914f5a521248f7761eb4d4e2ab496ba6db36dc
ashleynicole472 <asha.4720@gmail.com>
2018-05-10 11:11:08 -0700

    Created component, container and hoc folder

---
+++ burger-app/src/Components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../hoc/Aux'
+
+const layout = (props) => (
+  // Aux is created to allow this file to have a wrapping root element
+  <Aux>
+    <div>Toolbar, sideDrawer, backDrop</div>
+    <main>
+      [props.children]
+    </main>
+  </Aux>
+);
+
+export default layout;
