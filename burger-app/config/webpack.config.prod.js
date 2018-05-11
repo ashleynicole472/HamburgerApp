81ef12398143b027af81077334853dd6581cfcb1
ashleynicole472 <asha.4720@gmail.com>
2018-05-11 10:01:00 -0700

    created burger ingredient component, and was able to hard code and render burger to the DOM

--- burger-app/config/webpack.config.prod.js
+++ burger-app/config/webpack.config.prod.js
@@ -183,7 +183,7 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
-                        moduels: true,
+                        modules: true,
                         localIdentName: '[name]__[local]__[hash:base64:5]'
                       },
                     },
--- burger-app/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/Aux';
+import Burger from '../../Components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ingredient: {
+        salad: 1,
+        meat: 1,
+        bacon: 2,
+        cheese:2,
+      }
+    }
+  }
   render(){
     return(
       <Aux>
-        <div> Burger
-        </div>
-        <div>Build Controls
-        </div>
+        <Burger />
+        <div>Build Controls</div>
       </Aux>
     );
   }
