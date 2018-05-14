3325b0bffceb2e003f2989a26f799f841b13819d
ashleynicole472 <asha.4720@gmail.com>
2018-05-14 14:12:07 -0700

    Added functionality to the remove item button. Fixed styling again.

--- burger-app/src/Components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-app/src/Components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import classes from './BuildControl.css'
 
-
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.label}>{props.label} : </div>
-    <button className={classes.Less}>Less</button>
-    <button className={classes.More} onClick={props.added} onClick={props.calories}>More</button>
+    <button className={classes.Less} onClick={props.removed}>Less</button>
+    <button className={classes.More} onClick={props.added}>More</button>
   </div>
 )
 
--- burger-app/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -27,7 +27,7 @@ class BurgerBuilder extends Component {
         bacon: 0,
         cheese:0,
       },
-      totalPrice: 5,
+      totalPrice: 3,
       totalCal: 120,
     }
   }
@@ -60,7 +60,17 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) => {
-
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount-1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = this.state.oldprice - priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    console.log(this.state.totalPrice);
   }
 
   render(){
@@ -70,6 +80,7 @@ class BurgerBuilder extends Component {
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
           caloriesAdded={this.addCaloriesHandler} />
       </Aux>
     );
